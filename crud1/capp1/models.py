4ec9d9edc779f68d487f0a7a2935cfb4bc81d9b3
manomay <pachaderahul1@gmail.com>
2022-11-08 04:43:58 +0530

    create model"

--- crud1/capp1/models.py
+++ crud1/capp1/models.py
@@ -12,6 +12,15 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+class User(models.Model):
+    name = models.CharField(max_length =10)
+    address = models.CharField(max_length = 30)
+    phon_NO = models.CharField(max_length= 10)
+    create_at = models.DateTimeField(auto_now_add = True)
+    update_at = models.DateTimeField(auto_now = True)
+    email_add = models.EmailField()
 
+    def __str__(self):
+        return self.name
 
 
